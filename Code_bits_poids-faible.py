b8527e5afa0ce1a0da2c8d8a3500ac1e3454a110
Orion0304 <99364686+Orion0304@users.noreply.github.com>
2025-06-14 12:09:19 +0200

    Add files via upload

---
+++ Code_bits_poids-faible.py
@@ -0,0 +1,31 @@
+import wave
+
+fr="440.wav"
+
+'''Moitié forte des octets de poids faible modifiée, message répétés tant qu'il y a du son et chaque lettre du message est répétée nbrep fois.'''
+
+
+def codefaible(nbrep, nmfichier, message):
+    f_r = open(nmfichier, "rb")     #ouverture d'un fichier de lecture wave (fichier original)
+    f_w = open("440-1.wav", "wb")   #ouverture d'un fichier d'écriture wave
+
+    Wave_write.setparams(f_w, Wave_read.getparams(f_r)) #écriture de l'en-tête du fichier modifié
+
+    #nombre d'échantillons
+    n = Wave_read.getnframes(f_r)
+
+    #Codage 2 à 2 de la nouvelle séquence d'octets
+    k = 0
+    for k in range(n//(2*nbrep)): #n//2 doubles échantillons
+        lettre = message[k % len(message)]
+        for ite in range(nbrep):
+            seq = Wave_read.readframes(f_r, 2)  #lecture de 2 échantillons consécutifs
+            b = ord(lettre)
+            debut = bytes([(b//(2**3))*(2**3)])
+            fin = bytes([(b % (2**3))*(2**3)])
+            seq2 = (debut, bytes([seq[1]]), fin, bytes([seq[3]]))   #nouvelle séquence en laissant les octets de poids fort tel quel
+            for i in range(4):                                      #écriture de la séquence modifiée
+                Wave_write.writeframes(f_w, seq2[i])
+
+    Wave_read.close(f_r)    #fermeture du fichier de lecture
+    Wave_write.close(f_w)   #fermeture du fichier d'écriture
